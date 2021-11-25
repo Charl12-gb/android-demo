647be260847ad3393cd2765c040ee89fdd410064
Charles GBOYOU <gboyoucharles22@gmail.com>
2021-11-25 11:41:40 +0100

    Creation de l'activité(#3)

---
+++ app/src/main/java/Charl12_gb/demo/CalculatriceActivity.java
@@ -0,0 +1,14 @@
+package Charl12_gb.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
